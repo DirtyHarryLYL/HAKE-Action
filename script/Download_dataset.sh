d73624efcf4174bd6e9204535377419b875453c1
Yubing <yubinnng@gmail.com>
2020-09-03 17:22:46 +0800

    Add 'mkdir Weights/'
    Before downloading files in Weights, we should 'mkdir Weights/'

--- script/Download_dataset.sh
+++ script/Download_dataset.sh
@@ -16,6 +16,8 @@ python script/Download_data.py 1cqaJxzFRrMENTYGcuZFYy8JRuTP_qAEq Data/Trainval_N
 python script/Download_data.py 1KnQvUklj8p_cld1FagsUK6BEUeHqBtpR Data/Test_all_part.pkl
 python script/Download_data.py 1hkakTuYB_3C4GCbZgSm2pH7AHbhmEr5y Data/ava_train_all_fixed.pkl
 python script/Download_data.py 1cYR2rI8H78sY7exv-L3HXYhXqp7HQdUU Data/ava_val_fixed.pkl
+
+mkdir Weights/
 python script/Download_data.py 1ew-uIrD_ZFmO5RFsuMV2sBEqWWVtXkHH Weights/pasta_full.zip
 python script/Download_data.py 1h0InSjxCffLuyoXEPbLIzOH0KVWUuUJc Weights/pasta_AVA.zip
 python script/Download_data.py 1l12Dr217NeTnbBA5e-9lLetMFmDv7eI9 Weights/pasta_HICO-DET.zip
